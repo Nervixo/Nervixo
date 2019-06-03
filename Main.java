1d7c0d544129e3bdd8ace1c6b3b24b4415a7d630
Nervixo <49566735+Nervixo@users.noreply.github.com>
2019-06-03 14:28:40 -0400

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,25 @@
+package me.nervixo.BladeBot;
+
+import javax.security.auth.login.LoginException;
+
+import me.nervixo.BladeBot.Commands.Clear;
+import me.nervixo.BladeBot.Commands.Mute;
+import net.dv8tion.jda.core.AccountType;
+import net.dv8tion.jda.core.JDA;
+import net.dv8tion.jda.core.JDABuilder;
+import net.dv8tion.jda.core.OnlineStatus;
+import net.dv8tion.jda.core.entities.Game;
+
+public class Main {
+	public static JDA jda;
+	public static String prefix = "&";
+	
+	public static void main(String[] args) throws LoginException {
+		jda = new JDABuilder(AccountType.BOT).setToken("NTg1MTQwNTkyNDg5NTI5NTM2.XPVIxQ.pMAJRy2C3PuVj_F6Bbj0MAV360E").build();
+		jda.getPresence().setStatus(OnlineStatus.DO_NOT_DISTURB);
+		jda.getPresence().setGame(Game.playing("BladeMC"));
+		
+		jda.addEventListener(new Clear());
+		jda.addEventListener(new Mute());
+	}
+}
